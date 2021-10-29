75da2b44bfad1f2cb395ca8bd2b8b75e9801b44b
Andrew Naoya McWilliam <73796494+MurasatoNaoya@users.noreply.github.com>
2021-10-30 00:53:13 +0100

    Update Twitter Cryptocurrency Sentiment Analysis.py

--- Twitter Cryptocurrency Sentiment Analysis.py
+++ Twitter Cryptocurrency Sentiment Analysis.py
@@ -2,15 +2,22 @@
 # In the aim of getting a better understanding of the impression or 'sentiment' users have of certain cryptocurrencies. 
 
 
-
 # Import the appropriate libraries 
 
 import tweepy 
-from textblob import TextBlob 
-import pandas as pd 
-import numpy as np 
-import re 
-import matplotlib.pyplot as plt 
-plt.style.use('seaborn')  # Plotting style is preference here, anything from default to fivethirtyeight is fine. 
+from textblob import TextBlob
+import sys
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import os
+import nltk
+import pycountry
+import re
+import string
+
+
+
 
 
