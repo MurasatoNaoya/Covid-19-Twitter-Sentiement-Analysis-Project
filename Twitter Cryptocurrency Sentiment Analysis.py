2d85a3479e21169e4ccf1f95fbd89fe734a368c2
Andrew Naoya McWilliam <73796494+MurasatoNaoya@users.noreply.github.com>
2021-11-01 17:56:25 +0000

    Update Twitter Cryptocurrency Sentiment Analysis.py

--- Twitter Cryptocurrency Sentiment Analysis.py
+++ Twitter Cryptocurrency Sentiment Analysis.py
@@ -30,10 +30,10 @@ from sklearn.feature_extraction.text import CountVectorizer
 # Next we will need the consumer API key, the consumer API secret, access token and access token secret. 
 # I rec
 
-Oauth1_consumer_sey = AVudPPS0nePRUgOAWNOOyQZB4
-oauth1_consumer_secret = KeATWWKKjZYcJr9OExhMq1t2fKH35j5Z8hmPNwKOIg3EhwBHKv
-oauth1_access_token = 1454520951136194564-dXDgv1tI9EeRvMysAI1xpJxjpDW9hg
-oauth1_access_token_secret =  ByCcAp1zRDt2AuGCsu4astZtp78dBYDqjxQfh7G0dia7N
+Oauth1_consumer_sey = "AVudPPS0nePRUgOAWNOOyQZB4"
+oauth1_consumer_secret = "KeATWWKKjZYcJr9OExhMq1t2fKH35j5Z8hmPNwKOIg3EhwBHKv"
+oauth1_access_token = "1454520951136194564-dXDgv1tI9EeRvMysAI1xpJxjpDW9hg"
+oauth1_access_token_secret = "ByCcAp1zRDt2AuGCsu4astZtp78dBYDqjxQfh7G0dia7N"
 
 
 
