8480730a0b2cb8cc16acf31b414041977e41b867
Andrew Naoya McWilliam <73796494+MurasatoNaoya@users.noreply.github.com>
2022-01-13 20:01:22 +0000

    Update TwitterCovid19PolicySentimentAnalysis.py

--- TwitterCovid19PolicySentimentAnalysis.py
+++ TwitterCovid19PolicySentimentAnalysis.py
@@ -11,7 +11,7 @@ import sys
 import re
 
 import matplotlib.pyplot as plt   # Here we used the 'as' keyword to create an alias for the module we want to import. 
-plt.style.use(seaborn-bright) # What style I;ve chosen doesn't really matter, I have chosen 'seaborn-bright', but you could have anything. E.g fivethirtyeight. 
+plt.style.use(seaborn-bright) # What style chosen here doesn't really matter, I have chosen 'seaborn-bright' but you could have anything. E.g fivethirtyeight. 
 import pandas as pd
 import numpy as np
 
@@ -35,25 +35,22 @@ authentication = tweepy.OAuthHandler(Oauth1_consumer_key, oauth1_consumer_secret
 authentication.set_access_token(oauth1_access_token, oauth1_access_token_secret)
 
 # Creating the API object, that accounts for our authentication information - 
-# The Twitter API has a rate limit of 900 requests per 15 minutes, it would return anything above this amount an error. 
-# The 'wait_on_rate_limit' parameter asks whether or not automatically wait for rate limits to replenish, in this case we set it to 'True' to avoid errors.  
+# The Twitter API has a rate limit of 900 requests per 15 minutes, it would return an error for anything above this amount. 
+# The 'wait_on_rate_limit' parameter asks whether or not to automatically wait for rate limits to replenish, in this case we set it to 'True' to avoid any errors.  
 
 api = tweepy.API(authentication, wait_on_rate_limit= True)
-
-
-
 
 # Now that we have authentication, we can now use the tweepy and TextBlob modules to retrieve Tweets and discern intention parameters, respectively.
 
 
 # The below self defined function is to be used later to calculate the percentage of positive, negative and neutral Tweets we have in our sample.
-# The 'part' paramater representing whatever category of sentiment was want to look at and the 'whole' parameter representing our total number of Tweets, NoOfTweets. 
+# The 'part' paramater representing whatever category of sentiment we want to look at and the 'whole' parameter representing our total number of Tweets - NoOfTweets. 
 
 def percentage(part,whole):
    return 100 * float(part)/float(whole)
  
 
-# Recording the keyword(s) and number of Tweets being considered - the input() may not be called unless you are using an interactive interface like Juypter. 
+# Recording the keyword(s) and number of Tweets being considered
   
 keyword = input("Please enter keyword or hashtag to search: ")
 NoOfTweets = int(input ("Please enter how many tweets to analyse: "))
@@ -63,11 +60,15 @@ NoOfTweets = int(input ("Please enter how many tweets to analyse: "))
 
 tweets = tweepy.Cursor(api.search, q = keyword).items(NoOfTweets) # [Explain the usage of these function unique to tweepy]
 
-# Below are neutral lists, where the analysed tweets will be appended to - 
+# Below are baseline counters that will be added to in order to keep track of the number of each tweet with different sentiment. 
+# As well as neutral lists, where 'tweet_list' is all of the gathered tweets and all the others are Tweet lists that have been sorted by sentiment - 
+
 positive = 0
-positive = 0
+negative = 0
 neutral = 0
 
+# In the context of TextBlob, polarity indicates is a float in the range [-1-1], where 1 indicates a purely positive statement and -1 and purely negative statement.
+# So polarity will be measured for each iteration to keep account for the extent to which a tweet is positive or negative. 
 polarity = 0
 
 tweet_list = []
@@ -77,19 +78,18 @@ positive_list = []
 
 
 
-for tweet in tweets: # Essentially, for every Tweet in the list of int(NoOfTweets) Tweets that our API has scraped.. 
-   tweet_list.append(tweet.text)
+for tweet in tweets: # Essentially, for every Tweet in the list of int(NoOfTweets) Tweets that our API has scraped. 
+   tweet_list.append(tweet.text) # Add the Tweet text data to tweet_list 
 
- 
-   # We can now make our first 'TextBlob' using the TextBlob module, (Refer to this difficulty in the README file, linking the TextBlob Documentation.)
-   # This is in the for loop, so each individual tweet will be treated as a TextBlob to be processed. 
+# We can now make our first 'TextBlob' using the TextBlob module, (Refer to this difficulty in the README file, linking the TextBlob Documentation.)
+# This is in the for loop, so each individual tweet will be treated as a TextBlob to be processed. 
    analysis = TextBlob(tweet.text)
 
 
 
 
 # It is important to note that there are specific pieces of terminology unique to each library. 
-# In the context of TextBlob, polarity indicates is a float in the range [-1-1], where 1 indicates a purely positive statement and -1 and purely negative statement.
+
 
 
 
